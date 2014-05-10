89c66d5f44375a56ea518567ee4d0541fe2048b7
cilsilver <cilsilver@gmail.com>
2014-05-10 14:37:04 -0700

    Update database.php
    change my pass

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'commerce',
 			'username'  => 'root',
-			'password'  => 'alohomora',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
